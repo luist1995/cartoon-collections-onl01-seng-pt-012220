9301cca70adffa4f0a63340a1a05157842faf3c1
Luis F Tavarez Valdez <luis.tavarez1995@yahoo.com>
2020-02-01 23:36:47 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.map.with_index do |a,index| 
+    puts "#{index + 1}. #{a}"
+end
+end 
+
+def summon_captain_planet(array)
+  array.map do |a|
+    a.capitalize << "!"
+end
+end 
+
+def long_planeteer_calls(array)
+  array.any? { |word| word.length > 4}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
-end
-
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+  array.find do |a|
+    cheese_types.include?(a)
+  end
+  end
+
